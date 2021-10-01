2b1f6856cee42efad2b66e77582c5cbe27619e0e
Eduardo Piaia <eduardopiaia51@gmail.com>
2021-09-30 21:48:04 -0300

    Implementação da classe Cidade

---
+++ VendaDeVeiculos/Model/Cidade.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendaDeVeiculos.Model
+{
+    class Cidade
+    {
+        private int? cidId;
+        private string cidNome;
+        private string cidPais;
+        private string cidUf;
+
+        public Cidade()
+        {}
+
+        public Cidade(int? id, string nome, string pais, string uf)
+        {
+            this.cidId = id;
+            this.cidNome = nome;
+            this.cidPais = pais;
+            this.cidUf = uf;
+        }
+
+        public int? getCidId()
+        {
+            return this.cidId;
+        }
+
+        public void setCidId(int? id)
+        {
+            this.cidId = id;
+        }
+
+        public string getCidNome()
+        {
+            return this.cidNome;
+        }
+
+        public void setCidNome(string nome)
+        {
+            this.cidNome = nome;
+        }
+
+        public string getCidPais()
+        {
+            return this.cidPais;
+        }
+
+        public void setCidPais(string pais)
+        {
+            this.cidPais = pais;
+        }
+
+        public string getCidUf()
+        {
+            return this.cidUf;
+        }
+
+        public void setCidUf(string uf)
+        {
+            this.cidUf = uf;
+        }
+
+
+    }
+}
