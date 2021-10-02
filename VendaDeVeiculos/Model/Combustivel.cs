04d1e3577718ffc0c1fb2ae5d84014c265c4a1cd
Eduardo Piaia <eduardopiaia51@gmail.com>
2021-10-01 22:59:06 -0300

    Implementação das classes Cliente, Linha, Combustivel e Tipo e adaptação do encapsulamento para C#

---
+++ VendaDeVeiculos/Model/Combustivel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendaDeVeiculos.Model
+{
+    class Combustivel
+    {
+
+        private int? comId;
+        private string comNome;
+
+        public int? ComId { get => comId; set => comId = value; }
+        public string ComNome { get => comNome; set => comNome = value; }
+
+        public Combustivel()
+        {
+        }
+
+        public Combustivel(int? comId, string comNome)
+        {
+            this.comId = comId;
+            this.comNome = comNome;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Combustivel combustivel &&
+                   comId == combustivel.comId &&
+                   comNome == combustivel.comNome;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1788800486;
+            hashCode = hashCode * -1521134295 + comId.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(comNome);
+            return hashCode;
+        }
+    }
+}
---
+++ VendaDeVeiculos/Model/Tipo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendaDeVeiculos.Model
+{
+    class Tipo
+    {
+        private int? tipId;
+        private string tipNome;
+
+        public int? TipId { get => tipId; set => tipId = value; }
+        public string TipNome { get => tipNome; set => tipNome = value; }
+
+        public Tipo()
+        {
+        }
+
+        public Tipo(int? tipId, string tipNome)
+        {
+            this.tipId = tipId;
+            this.tipNome = tipNome;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Tipo tipo &&
+                   tipId == tipo.tipId;
+        }
+
+        public override int GetHashCode()
+        {
+            return 199592255 + tipId.GetHashCode();
+        }
+    }
+}
